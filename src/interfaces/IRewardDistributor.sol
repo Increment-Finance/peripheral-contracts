94df5977679f85c86814b8923328f924a7030ed6
webthethird <ratzbodell@gmail.com>
2023-09-15 17:32:56 -0400

    Fix NatSpec missing params, formatting

--- src/interfaces/IRewardDistributor.sol
+++ src/interfaces/IRewardDistributor.sol
@@ -7,9 +7,9 @@ import {IClearingHouse} from "increment-protocol/interfaces/IClearingHouse.sol";
 import {IPerpetual} from "increment-protocol/interfaces/IPerpetual.sol";
 
 interface IRewardDistributor {
-
     /// Emitted when rewards are accrued to an LP
     /// @param lp Address of the liquidity provier
+    /// @param rewardToken Address of the reward token
     /// @param perpetual Address of the perpetual market
     /// @param reward Amount of reward accrued
     event RewardAccrued(
@@ -21,6 +21,7 @@ interface IRewardDistributor {
 
     /// Emitted when an LP claims their accrued rewards
     /// @param lp Address of the liquidity provier
+    /// @param rewardToken Address of the reward token
     /// @param reward Amount of reward claimed
     event RewardClaimed(
         address indexed lp,
@@ -28,23 +29,27 @@ interface IRewardDistributor {
         uint256 reward
     );
 
-    /// Emitted when an LP's position is changed in the reward distributor 
+    /// Emitted when an LP's position is changed in the reward distributor
     /// @param lp Address of the liquidity provier
     /// @param perpetualIndex Index of the perpetual market in the ClearingHouse
     /// @param prevPosition Previous LP position of the user
     /// @param newPosition New LP position of the user
     event PositionUpdated(
-        address indexed lp, 
-        uint256 perpetualIndex, 
-        uint256 prevPosition, 
+        address indexed lp,
+        uint256 perpetualIndex,
+        uint256 prevPosition,
         uint256 newPosition
     );
 
     function earlyWithdrawalThreshold() external view returns (uint256);
 
     function registerPositions() external;
+
     function registerPositions(uint256[] calldata) external;
+
     function claimRewards() external;
+
     function claimRewardsFor(address) external;
+
     function claimRewardsFor(address, address[] memory) external;
 }
