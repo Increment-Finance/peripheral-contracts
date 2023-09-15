554c0fca953e02f6973d895a1d176a254a7dc5d6
webthethird <ratzbodell@gmail.com>
2023-09-15 17:34:58 -0400

    Fix NatSpec missing params, formatting

--- src/interfaces/IGaugeController.sol
+++ src/interfaces/IGaugeController.sol
@@ -18,6 +18,7 @@ interface IGaugeController {
 
     /// Emitted when governance removes a reward token
     /// @param rewardToken the reward token address
+    /// @param unclaimedRewards the amount of reward tokens still claimable
     /// @param remainingBalance the remaining balance of the reward token, sent to governance
     event RewardTokenRemoved(
         address indexed rewardToken,
@@ -27,31 +28,35 @@ interface IGaugeController {
 
     /// Emitted when a gauge weight is updated
     /// @param gauge the address of the perp market or safety module (i.e., gauge)
+    /// @param rewardToken the reward token address
     /// @param newWeight the new weight value
     event NewWeight(
-        address indexed gauge, 
+        address indexed gauge,
         address indexed rewardToken,
         uint16 newWeight
     );
 
     /// Emitted when a new inflation rate is set by governance
     /// @param newRate the new inflation rate
-    event NewInflationRate(
-        address indexed rewardToken,
-        uint256 newRate
-    );
+    event NewInflationRate(address indexed rewardToken, uint256 newRate);
 
     /// Emitted when a new reduction factor is set by governance
     /// @param newFactor the new reduction factor
-    event NewReductionFactor(
-        address indexed rewardToken,
-        uint256 newFactor
-    );
+    event NewReductionFactor(address indexed rewardToken, uint256 newFactor);
+
     function clearingHouse() external view returns (IClearingHouse);
+
     function rewardTokens(uint256) external view returns (address);
 
+    function getNumGauges() external view returns (uint256);
+
+    function getGaugeAddress(uint256) external view returns (address);
+
     function updateMarketRewards(uint256) external;
+
     function updateGaugeWeights(address, uint16[] calldata) external;
+
     function updateInflationRate(address, uint256) external;
+
     function updateReductionFactor(address, uint256) external;
 }
