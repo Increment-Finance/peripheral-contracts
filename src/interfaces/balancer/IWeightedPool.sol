047c8e68362ee0ec005268816cdf08c02a16f2f0
webthethird <ratzbodell@gmail.com>
2023-10-03 15:51:35 -0400

    Add Balancer interfaces for testing

---
+++ src/interfaces/balancer/IWeightedPool.sol
@@ -0,0 +1,233 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "./IVault.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+
+interface IBalancerPoolToken is IERC20, IERC20Permit {
+    function getVault() external view returns (IVault);
+}
+
+interface IRateProvider {
+    /**
+     * @dev Returns an 18 decimal fixed point number that is the exchange rate of the token to some other underlying
+     * token. The meaning of this rate depends on the context.
+     */
+    function getRate() external view returns (uint256);
+}
+
+interface IPoolSwapStructs {
+    // This is not really an interface - it just defines common structs used by other interfaces: IGeneralPool and
+    // IMinimalSwapInfoPool.
+    //
+    // This data structure represents a request for a token swap, where `kind` indicates the swap type ('given in' or
+    // 'given out') which indicates whether or not the amount sent by the pool is known.
+    //
+    // The pool receives `tokenIn` and sends `tokenOut`. `amount` is the number of `tokenIn` tokens the pool will take
+    // in, or the number of `tokenOut` tokens the Pool will send out, depending on the given swap `kind`.
+    //
+    // All other fields are not strictly necessary for most swaps, but are provided to support advanced scenarios in
+    // some Pools.
+    //
+    // `poolId` is the ID of the Pool involved in the swap - this is useful for Pool contracts that implement more than
+    // one Pool.
+    //
+    // The meaning of `lastChangeBlock` depends on the Pool specialization:
+    //  - Two Token or Minimal Swap Info: the last block in which either `tokenIn` or `tokenOut` changed its total
+    //    balance.
+    //  - General: the last block in which *any* of the Pool's registered tokens changed its total balance.
+    //
+    // `from` is the origin address for the funds the Pool receives, and `to` is the destination address
+    // where the Pool sends the outgoing tokens.
+    //
+    // `userData` is extra data provided by the caller - typically a signature from a trusted party.
+    struct SwapRequest {
+        IVault.SwapKind kind;
+        IERC20 tokenIn;
+        IERC20 tokenOut;
+        uint256 amount;
+        // Misc data
+        bytes32 poolId;
+        uint256 lastChangeBlock;
+        address from;
+        address to;
+        bytes userData;
+    }
+}
+
+interface IControlledPool {
+    function setSwapFeePercentage(uint256 swapFeePercentage) external;
+
+    function setAssetManagerPoolConfig(
+        IERC20 token,
+        bytes memory poolConfig
+    ) external;
+}
+
+interface IRecoveryMode {
+    /**
+     * @dev Emitted when the Recovery Mode status changes.
+     */
+    event RecoveryModeStateChanged(bool enabled);
+
+    /**
+     * @notice Enables Recovery Mode in the Pool, disabling protocol fee collection and allowing for safe proportional
+     * exits with low computational complexity and no dependencies.
+     */
+    function enableRecoveryMode() external;
+
+    /**
+     * @notice Disables Recovery Mode in the Pool, restoring protocol fee collection and disallowing proportional exits.
+     */
+    function disableRecoveryMode() external;
+
+    /**
+     * @notice Returns true if the Pool is in Recovery Mode.
+     */
+    function inRecoveryMode() external view returns (bool);
+}
+
+interface IBasePool is
+    IPoolSwapStructs,
+    IControlledPool,
+    IBalancerPoolToken,
+    ITemporarilyPausable,
+    IRecoveryMode
+{
+    /**
+     * @dev Called by the Vault when a user calls `IVault.joinPool` to add liquidity to this Pool. Returns how many of
+     * each registered token the user should provide, as well as the amount of protocol fees the Pool owes to the Vault.
+     * The Vault will then take tokens from `sender` and add them to the Pool's balances, as well as collect
+     * the reported amount in protocol fees, which the pool should calculate based on `protocolSwapFeePercentage`.
+     *
+     * Protocol fees are reported and charged on join events so that the Pool is free of debt whenever new users join.
+     *
+     * `sender` is the account performing the join (from which tokens will be withdrawn), and `recipient` is the account
+     * designated to receive any benefits (typically pool shares). `balances` contains the total balances
+     * for each token the Pool registered in the Vault, in the same order that `IVault.getPoolTokens` would return.
+     *
+     * `lastChangeBlock` is the last block in which *any* of the Pool's registered tokens last changed its total
+     * balance.
+     *
+     * `userData` contains any pool-specific instructions needed to perform the calculations, such as the type of
+     * join (e.g., proportional given an amount of pool shares, single-asset, multi-asset, etc.)
+     *
+     * Contracts implementing this function should check that the caller is indeed the Vault before performing any
+     * state-changing operations, such as minting pool shares.
+     */
+    function onJoinPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        uint256[] memory balances,
+        uint256 lastChangeBlock,
+        uint256 protocolSwapFeePercentage,
+        bytes memory userData
+    )
+        external
+        returns (
+            uint256[] memory amountsIn,
+            uint256[] memory dueProtocolFeeAmounts
+        );
+
+    /**
+     * @dev Called by the Vault when a user calls `IVault.exitPool` to remove liquidity from this Pool. Returns how many
+     * tokens the Vault should deduct from the Pool's balances, as well as the amount of protocol fees the Pool owes
+     * to the Vault. The Vault will then take tokens from the Pool's balances and send them to `recipient`,
+     * as well as collect the reported amount in protocol fees, which the Pool should calculate based on
+     * `protocolSwapFeePercentage`.
+     *
+     * Protocol fees are charged on exit events to guarantee that users exiting the Pool have paid their share.
+     *
+     * `sender` is the account performing the exit (typically the pool shareholder), and `recipient` is the account
+     * to which the Vault will send the proceeds. `balances` contains the total token balances for each token
+     * the Pool registered in the Vault, in the same order that `IVault.getPoolTokens` would return.
+     *
+     * `lastChangeBlock` is the last block in which *any* of the Pool's registered tokens last changed its total
+     * balance.
+     *
+     * `userData` contains any pool-specific instructions needed to perform the calculations, such as the type of
+     * exit (e.g., proportional given an amount of pool shares, single-asset, multi-asset, etc.)
+     *
+     * Contracts implementing this function should check that the caller is indeed the Vault before performing any
+     * state-changing operations, such as burning pool shares.
+     */
+    function onExitPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        uint256[] memory balances,
+        uint256 lastChangeBlock,
+        uint256 protocolSwapFeePercentage,
+        bytes memory userData
+    )
+        external
+        returns (
+            uint256[] memory amountsOut,
+            uint256[] memory dueProtocolFeeAmounts
+        );
+
+    /**
+     * @dev Returns this Pool's ID, used when interacting with the Vault (to e.g. join the Pool or swap with it).
+     */
+    function getPoolId() external view returns (bytes32);
+
+    /**
+     * @dev Returns the current swap fee percentage as a 18 decimal fixed point number, so e.g. 1e17 corresponds to a
+     * 10% swap fee.
+     */
+    function getSwapFeePercentage() external view returns (uint256);
+
+    /**
+     * @dev Returns the scaling factors of each of the Pool's tokens. This is an implementation detail that is typically
+     * not relevant for outside parties, but which might be useful for some types of Pools.
+     */
+    function getScalingFactors() external view returns (uint256[] memory);
+
+    function queryJoin(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        uint256[] memory balances,
+        uint256 lastChangeBlock,
+        uint256 protocolSwapFeePercentage,
+        bytes memory userData
+    ) external returns (uint256 bptOut, uint256[] memory amountsIn);
+
+    function queryExit(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        uint256[] memory balances,
+        uint256 lastChangeBlock,
+        uint256 protocolSwapFeePercentage,
+        bytes memory userData
+    ) external returns (uint256 bptIn, uint256[] memory amountsOut);
+}
+
+interface IMinimalSwapInfoPool is IBasePool {
+    function onSwap(
+        SwapRequest memory swapRequest,
+        uint256 currentBalanceTokenIn,
+        uint256 currentBalanceTokenOut
+    ) external returns (uint256 amount);
+}
+
+interface IBaseWeightedPool is IMinimalSwapInfoPool {
+    function getInvariant() external view returns (uint256);
+
+    function getNormalizedWeights() external view returns (uint256[] memory);
+}
+
+interface IWeightedPoolProtocolFees is IBaseWeightedPool {
+    function getLastPostJoinExitInvariant() external view returns (uint256);
+
+    function getATHRateProduct() external view returns (uint256);
+
+    function getRateProviders() external view returns (IRateProvider[] memory);
+}
+
+interface IWeightedPool is IBaseWeightedPool {
+    function getActualSupply() external view returns (uint256);
+}
