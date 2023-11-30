d8e54f478169cd3273c2c1ae0f3d3d13f8899773
webthethird <ratzbodell@gmail.com>
2023-11-30 12:55:51 -0500

    Refactor arg name in interface

--- contracts/interfaces/IRewardDistributor.sol
+++ contracts/interfaces/IRewardDistributor.sol
@@ -211,8 +211,8 @@ interface IRewardDistributor {
     function removeRewardToken(address _rewardToken) external;
 
     /// @notice Updates the address of the ecosystem reserve for storing reward tokens
-    /// @param _ecosystemReserve Address of the new ecosystem reserve
-    function setEcosystemReserve(address _ecosystemReserve) external;
+    /// @param _newEcosystemReserve Address of the new ecosystem reserve
+    function setEcosystemReserve(address _newEcosystemReserve) external;
 
     /// @notice Sets the start time for accruing rewards to a market which has not been initialized yet
     /// @param _market Address of the market (i.e., perpetual market or staking token)
