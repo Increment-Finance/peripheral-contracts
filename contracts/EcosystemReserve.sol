f213aa4c671d85e5a6474a44df15331225bc5ff1
webthethird <ratzbodell@gmail.com>
2023-12-14 15:43:41 -0500

    Move AdminControlledEcosystemReserve to ./contracts/ and tweak imports to avoid needing to `npm install` in @aave-v3-periphery submodule

--- contracts/EcosystemReserve.sol
+++ contracts/EcosystemReserve.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.16;
 
-import "@aave-periphery/treasury/AdminControlledEcosystemReserve.sol";
+import "./AdminControlledEcosystemReserve.sol";
 
 /// @title EcosystemReserve
 /// @author webthethird
