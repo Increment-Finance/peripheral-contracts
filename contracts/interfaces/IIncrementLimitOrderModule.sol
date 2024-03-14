f18e8ad376d75390c1194f1778341592ad571f42
webthethird <ratzbodell@gmail.com>
2024-03-14 14:10:26 -0400

    Add `executeMarketOrder` to limit order module interface

--- contracts/interfaces/IIncrementLimitOrderModule.sol
+++ contracts/interfaces/IIncrementLimitOrderModule.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.16;
 
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./ILimitOrderBook.sol";
+import {LibPerpetual} from "@increment/lib/LibPerpetual.sol";
 
 interface IIncrementLimitOrderModule is IERC165 {
-    function executeLimitOrder(
-        ILimitOrderBook.LimitOrder memory order
-    ) external;
+    function executeLimitOrder(ILimitOrderBook.LimitOrder memory order) external;
+
+    function executeMarketOrder(uint256 marketIdx, uint256 amount, LibPerpetual.Side side) external;
 }
