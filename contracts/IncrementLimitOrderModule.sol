308d037b2e48c48469efda298d09fee94585ad64
webthethird <ratzbodell@gmail.com>
2024-04-15 14:24:42 -0400

    Upgrade to solidity 0.8.20 for compatibility with era-contracts

--- contracts/IncrementLimitOrderModule.sol
+++ contracts/IncrementLimitOrderModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 // contracts
 import {EIP712} from "clave-contracts/contracts/helpers/EIP712.sol";
--- contracts/interfaces/IIncrementLimitOrderModule.sol
+++ contracts/interfaces/IIncrementLimitOrderModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 // interfaces
 import {IModule} from "clave-contracts/contracts/interfaces/IModule.sol";
