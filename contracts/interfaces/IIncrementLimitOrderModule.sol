354be6a4e80b701633b39b77cfc17cd9d1d44b84
webthethird <ratzbodell@gmail.com>
2024-04-15 12:36:55 -0400

    IIncrementLimitOrderModule is IModule

--- contracts/interfaces/IIncrementLimitOrderModule.sol
+++ contracts/interfaces/IIncrementLimitOrderModule.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.16;
 
-import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {IModule} from "clave-contracts/contracts/interfaces/IModule.sol";
 import "./ILimitOrderBook.sol";
 import {LibPerpetual} from "@increment/lib/LibPerpetual.sol";
 
-interface IIncrementLimitOrderModule is IERC165 {
+interface IIncrementLimitOrderModule is IModule {
     function executeLimitOrder(ILimitOrderBook.LimitOrder memory order) external;
 
     function executeMarketOrder(uint256 marketIdx, uint256 amount, LibPerpetual.Side side) external;
