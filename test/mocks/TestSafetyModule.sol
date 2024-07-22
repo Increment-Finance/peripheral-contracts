570183168bbc590e6308e79b6d3716beeb7dd63a
webthethird <ratzbodell@gmail.com>
2024-07-22 12:16:40 -0400

    Replace `0.8.16` w/ `^0.8.16`

--- test/mocks/TestSafetyModule.sol
+++ test/mocks/TestSafetyModule.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
 import {SafetyModule} from "../../contracts/SafetyModule.sol";
 import {IStakedToken} from "../../contracts/interfaces/IStakedToken.sol";
